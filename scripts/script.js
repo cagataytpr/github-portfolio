5be04b2c905ec18988ee4f3e5b097acc5e493b41
Çağatay TUPUR <ctupur@sema.local>
2025-12-10 14:08:30 +0300

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,13 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-// YENİ FİLTRELEME FONKSİYONU
+
 function filterProjects(category) {
     const projects = document.querySelectorAll('.details-container');
     
-    // Tüm projeler üzerinde döngü yap
+    
     projects.forEach(project => {
-        // Eğer kategori 'all' ise veya projenin ilgili kategori sınıfı varsa göster
+        
         if (category === 'all' || project.classList.contains('category-' + category)) {
             project.style.display = 'flex'; 
         } else {
@@ -19,11 +19,11 @@ function filterProjects(category) {
         }
     });
 
-    // Butonun aktif görünmesini sağlamak için
+    
     const filterButtons = document.querySelectorAll('.filter-btn');
     filterButtons.forEach(button => {
         button.classList.remove('active-filter');
     });
-    // Tıklanan butona 'active-filter' sınıfını ekle
+    
     document.querySelector(`[onclick="filterProjects('${category}')"]`).classList.add('active-filter');
 }
